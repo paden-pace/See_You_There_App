059777bced3b31d660985102791035613bee1cce
Paden Pace <padenpace@wireless-10-147-22-181.public.utexas.edu>
2017-03-07 19:37:15 -0600

    adding Database and adjusting javaScript pages

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAOvuBjDloWxmel5DKtcKp2IpIjyPZDqp4",
+    authDomain: "groupproject-1-ftp.firebaseapp.com",
+    databaseURL: "https://groupproject-1-ftp.firebaseio.com",
+    storageBucket: "groupproject-1-ftp.appspot.com",
+    messagingSenderId: "355866249539"
+  };
+  firebase.initializeApp(config);
+
+  
